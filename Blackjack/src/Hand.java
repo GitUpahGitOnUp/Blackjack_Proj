36d5abd3ac3acf88bb1d62adfa9f62dfea9ceb3f
GitUpahGitOnUp <angelaahutchison@gmail.com>
2024-07-02 14:27:06 -0400

    added card deal method

--- Blackjack/src/Hand.java
+++ Blackjack/src/Hand.java
@@ -6,4 +6,9 @@ public class Hand {
     public Hand() {
         hand = new ArrayList<Card>();
     }
+
+    // method that takes a card from the deck and adds a card to a hand
+    public void takeCardFromDeck(Deck deck) {
+        hand.add(deck.takeCard());
+    }
 }
